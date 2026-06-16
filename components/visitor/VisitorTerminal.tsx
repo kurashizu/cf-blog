f267802e44f332b79abffdf3ef7eb9c9983bfcdc
kurashizu <kurashizu123@gmail.com>
2026-06-16 21:39:16 +1000

    style: shorten separator to match title length

--- components/visitor/VisitorTerminal.tsx
+++ components/visitor/VisitorTerminal.tsx
@@ -71,7 +71,7 @@ function buildDisplay(info: VisitorInfo, logoText: string): BuildResult {
 
     const infoLines: string[] = [
         cap("visitor@kurashizu-blog", MAX_RIGHT),
-        "─".repeat(MAX_RIGHT),
+        "─".repeat("visitor@kurashizu-blog".length),
         cap(`IP        ${maskIP(info.ip)}`, MAX_RIGHT),
         cap(`Location  ${loc}`, MAX_RIGHT),
         cap(`ISP       ${isp}`, MAX_RIGHT),
